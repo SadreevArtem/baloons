efa2bb384fd1d6368535f34b164b1afb4aa6d4d4
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-07-20 16:48:01 +0500

    fix margin iphone

--- shared/components/CatalogComponent/CatalogComponent.tsx
+++ shared/components/CatalogComponent/CatalogComponent.tsx
@@ -13,7 +13,7 @@ type Props = {
 export const CatalogComponent: React.FC<Props> = ({title, className}) => {
   return (
     <>
-      <section className={`w-full flex flex-col py-4 ${inter.className, className} bg-[#f7f7f7] ${styles.wrapper} min-h-[60vh] max-md:mb-8`}>
+      <section className={`w-full flex flex-col py-4 ${inter.className, className} bg-[#f7f7f7] ${styles.wrapper} min-h-[60vh]`}>
         <h3 className="container self-center text-center text-primary md:text-5xl text-2xl font-extrabold mb-6">{title}</h3>
         <ul className="container py-4 grid md:grid-cols-6 grid-cols-2 gap-6 md:gap-8 ">
             {CATALOG.map((category)=> <CategoryCard item={category} key={category.name}/>)}
